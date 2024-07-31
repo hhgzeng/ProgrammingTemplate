6702d2c7f2d7bad3482a68911766e4d289fa940f
hhgzeng <134180004+hhgzeng@users.noreply.github.com>
2024-07-31 23:53:58 +0800

    Add files via upload

---
+++ Int128.cpp
@@ -0,0 +1,9 @@
+std::ostream &operator<<(std::ostream &os, i128 n) {
+    std::string s;
+    while (n) {
+        s += '0' + n % 10;
+        n /= 10;
+    }
+    std::reverse(s.begin(), s.end());
+    return os << s;
+}
