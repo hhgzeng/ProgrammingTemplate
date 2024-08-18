97fe811d02427017330a0183078badf48b63135a
hhgzeng <134180004+hhgzeng@users.noreply.github.com>
2024-08-18 21:10:52 +0800

    Update RMQ.cpp

--- DataStructure/RMQ.cpp
+++ DataStructure/RMQ.cpp
@@ -20,7 +20,7 @@ struct RMQ {
             return;
         }
         const int M = (n - 1) / B + 1;
-        const int lg = std::__lg(M);
+        const int lg = int(std::log2(M));
         a.assign(lg + 1, std::vector<T>(M));
         for (int i = 0; i < M; i++) {
             a[0][i] = v[i * B];
@@ -48,8 +48,8 @@ struct RMQ {
             const int r = std::min(1U * n, l + B);
             u64 s = 0;
             for (int j = l; j < r; j++) {
-                while (s && cmp(v[j], v[std::__lg(s) + l])) {
-                    s ^= 1ULL << std::__lg(s);
+                while (s && cmp(v[j], v[int(std::log2(s)) + l])) {
+                    s ^= 1ULL << int(std::log2(s));
                 }
                 s |= 1ULL << (j - l);
                 stk[j] = s;
@@ -62,7 +62,7 @@ struct RMQ {
             l = l / B + 1;
             r = r / B;
             if (l < r) {
-                int k = std::__lg(r - l);
+                int k = int(std::log2(r - l));
                 ans = std::min({ans, a[k][l], a[k][r - (1 << k)]}, cmp);
             }
             return ans;
